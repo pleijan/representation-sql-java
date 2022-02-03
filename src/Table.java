33ad3b0d6fccf51ac419ee83b62b4dcf409b6548
pleijan <72879538+pleijan@users.noreply.github.com>
2022-02-03 15:29:39 -0500

    ADD :Table Javadocs

--- src/Table.java
+++ src/Table.java
@@ -5,30 +5,38 @@ public class Table {
     private ArrayList<String> elements;
     private String name;
 
-
+    /**
+     * constructor of the table class with no element
+     * @param nameTable name of the table
+     */
     public void Table(String nameTable){
         name = nameTable;
         elements = new ArrayList<String>();
     }
 
-    public void Table(String nameTable,String[] initElements){
-        name = nameTable;
-        elements = new ArrayList<String>();
-        for(int i=0; i<initElements.length; i++){
-            elements.add(initElements[i]);
-        }
-    }
 
+    /**
+     * function who add one element to the list "Elements"
+     * @param newElement
+     */
     public void addElement(String newElement){
         elements.add(newElement);
     }
 
+    /**
+     * function who add a list of elements to the list "Elements"
+     * @param newElements
+     */
     public void addElement(String[] newElements){
         for(int i=0; i<newElements.length; i++){
             elements.add(newElements[i]);
         }
     }
 
+    /**
+     * function who remove one element from the list "Elements"
+     * @param toRemove
+     */
     public void removeElement(String toRemove){
         if (!elements.remove(toRemove)) {
             System.err.println(
@@ -36,6 +44,10 @@ public class Table {
         }
     }
 
+    /**
+     * function who return the name of the table
+     * @return the name of the table
+     */
     public String getName(){
         return name;
     }
