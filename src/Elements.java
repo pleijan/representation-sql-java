80d0a7602dc4b21d45709f1a127badb7d8ec49df
pleijan <72879538+pleijan@users.noreply.github.com>
2022-02-03 13:49:31 -0500

    SETUP : Files

---
+++ src/Elements.java
@@ -0,0 +1,17 @@
+public class Elements {
+    String elements[];
+
+    public Elements(String elements[]){
+        this.elements = elements;
+    }
+
+    @Override
+    public String toString() {
+        String retour = "";
+        for (int i = 0; i<elements.length; i++){
+            retour += elements[i] + ", ";
+        }
+        retour += elements[elements.length];
+        return retour;
+    }
+}
