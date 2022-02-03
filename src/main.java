4bf915409bc0c3ca5fb758f8ba837fbc79c418fc
pleijan <72879538+pleijan@users.noreply.github.com>
2022-02-03 13:49:31 -0500

    SETUP : Files

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public void main(){
+        String elements[] = {"Nom", "Prenom"};
+        Elements voulus = new Elements(elements);
+
+        voulus.toString();
+    }
+}
