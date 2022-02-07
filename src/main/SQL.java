10f341a0110742b26b9a25fa6b391f195de688ba
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2022-02-07 11:02:25 -0500

    Beginning of SQL Class

---
+++ src/main/SQL.java
@@ -0,0 +1,55 @@
+package main;
+
+
+import main.queries.Query;
+import main.queries.Tables.*;
+
+/**
+ * Main class of the project
+ */
+public class SQL {
+    // OPERATOR
+    public static final String EQUAL = " = ";
+    public static final String NOTEQUAL = " <> ";
+    public static final String GT = " > ";
+    public static final String LT = " < ";
+    public static final String GTE = " >= ";
+    public static final String LTE = " <= ";
+    public static final String IN = " IN ";
+    public static final String BETWEEN = " BETWEEN";
+    public static final String LIKE = " LIKE ";
+    public static final String ISNULL = " IS NULL ";
+    public static final String NOTNULL = " IS NOT NULL ";
+
+
+    /**
+     * Fabric for Query element
+     * WIP
+     * @param _query the name of the query
+     * @return a Query object
+     */
+    public Query create(String _query) {
+        switch (_query.toLowerCase()) {
+
+            case "select":
+                return new Select();
+
+            case "drop":
+                return new Drop();
+
+            case "insert":
+                return new Insert();
+
+            case "update":
+                return new Update();
+/*
+            case "create":
+                return new Create();*/
+            default:
+                return null;
+        }
+    }
+
+}
+
+
